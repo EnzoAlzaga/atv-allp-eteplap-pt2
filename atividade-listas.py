28ba11bbf755e989a6b7cb2b1035190fe11758b6
EnzoAlzaga <enzoalzaga@hotmail.com>
2025-05-16 03:12:49 -0300

    Add files via upload

---
+++ atividade-listas.py
@@ -0,0 +1,20 @@
+animais = ["cachorro", "gato", "papagaio"]
+animais.append("hamster")
+animais.insert(1, "coelho")
+animais.pop()
+animais.pop(2)
+animais[0] = "leão"
+print("Lista final:", animais)
+
+
+lista = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+print("Este é o número acessado:", lista[1][1])
+
+
+lista2 = [10, 20, [30, 40], "hello"]
+print("O comprimento da lista:", len(lista2))
+
+
+frutas = ['morango', 'abacaxi', 'goiaba', 'jambo', 'banana']
+print("O primeiro elemento:", frutas[0])
+print("O terceiro elemento:", frutas[2])
